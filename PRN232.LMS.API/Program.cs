d462c3f9ee89ca964195185caf7062fa8e413e0e
GiaHuy361 <top1latao@gmail.com>
2026-05-23 14:47:26 +0700

    chore: restore Swagger Title to PRN232.LMS.API and Version to 1.0

--- PRN232.LMS.API/Program.cs
+++ PRN232.LMS.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddDbContext<LmsDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "PRN232 SE1815 SE182723 PLGH LMS API", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "PRN232.LMS.API", Version = "1.0" });
     c.OperationFilter<HideJsonIgnoreParameterFilter>(); // hides ExpandList / FieldList
     c.OperationFilter<QueryParameterDescriptionFilter>(); // lowercases and adds rich descriptions to query parameters
 });
@@ -44,7 +44,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PRN232 SE1815 SE182723 PLGH LMS API v1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PRN232.LMS.API v1");
     c.RoutePrefix = "swagger";
 });
 
